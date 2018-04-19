a5517197da0bf49cfa4e0556fc4bf50e81c5c063
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2018-04-19 20:35:15 +0900

    Fbx 系Import 設定も追加

---
+++ Editor/CustomFbxImporterScriptableObject.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class CustomFbxImporterScriptableObject : ScriptableObject
+{
+    //-------------------------------------
+    // 定数関連
+    //-------------------------------------
+    #region ===== CONSTS =====
+
+    const string DEFAULT_SCRIPTABLEOBJECT_GENERATED_PATH = "Assets/CustomAssetImportPreset/ScriptableObjects/";
+    const string DEFAULT_SCRIPTABLEOBJECT_FILENAME = "CustomFbxImporter.asset";
+
+    #endregion //) ===== CONSTS =====
+
+	/// <summary>
+	/// Model 関連のimport設定
+	/// </summary>
+    [System.Serializable]
+    public class ModelImportSettings
+    {
+		[Header("Meshes")]
+        public float ScaleFactor = 1.0f;
+		public bool UseFileScale = true;
+		public ModelImporterMeshCompression MeshCompression = ModelImporterMeshCompression.Off;
+		public bool ReadWriteEnabled = true;
+		public bool OptimizeMesh = true;
+		public bool ImportBlendShapes = true;
+		// public bool GenerateCollider = false;
+		public bool KeepQuads = false;
+		public bool WeldticesVer = true;
+		public bool ImportVisibly = true;
+		public bool ImportCameras = true;
+		public bool ImportLights = true;
+		public bool SwapUVs = false;
+		public bool GenerateLightmapUVs = false;
+
+		[Header("Normal & Tangents ")]
+		public ModelImporterNormals Normals = ModelImporterNormals.Import;
+		public ModelImporterTangents Tangents = ModelImporterTangents.Import;
+
+		[Header("Materials")]
+		public bool ImportMaterials = true;
+		public ModelImporterMaterialName MaterialName		=  ModelImporterMaterialName.BasedOnTextureName;
+		public ModelImporterMaterialSearch MaterialSearch	=  ModelImporterMaterialSearch.RecursiveUp;
+    }
+
+	/// <summary>
+	/// Rig関連のimport設定
+	/// </summary>
+    [System.Serializable]
+    public class RigImportSettings
+    {
+        public ModelImporterAnimationType AnimationType = ModelImporterAnimationType.Generic;
+		public bool OptimizeGameObjects = false;
+    }
+
+	/// <summary>
+	/// Animation関連のimport設定
+	/// </summary>
+    [System.Serializable]
+    public class AnimImportSettings
+    {
+		public bool ImportAnimation = true;
+    }
+
+	 //-------------------------------------
+    // MemberVariables
+    //-------------------------------------
+    #region ===== MEMBER_VARIABLES =====
+    
+    /// <summary>
+    ///  対象のPath
+    /// </summary>
+    [Tooltip("対象のpath")]
+    public string Path = "Assets/";
+
+    /// <summary>
+    /// 対象となるテクスチャの拡張子リスト
+    /// </summary>
+    [Tooltip("対象となる拡張子")]
+    public string[] ExtentionList = new string[]{".png"};
+
+    /// <summary>
+    /// Import設定
+    /// </summary>
+    public ModelImportSettings ModelImport = new ModelImportSettings();
+	public RigImportSettings RigImport = new RigImportSettings();
+	public AnimImportSettings AnimImport = new AnimImportSettings();
+
+    #endregion //) ===== MEMBER_VARIABLES =====
+    
+}
---
+++ Editor/CustomModelImporter.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+/// <summary>
+/// ScriptableObject で設定したImport設定でImportするツール
+/// </summary>
+public class CustomModelImporter : AssetPostprocessor
+{
+    void OnPreprocessTexture ()
+    {
+        CustomFbxImporterScriptableObject[] importers = GetAllTargetScriptableObject();
+
+        if( importers == null || importers.Length < 1 )
+        {
+            return;
+        }
+
+        CustomFbxImporterScriptableObject targetImporter = null; 
+        for (int i = 0; i < importers.Length; i++)
+        {
+            var item = importers[i];
+            // 対象のPathチェック
+            if( !assetPath.Contains( item.Path))
+            {
+                continue;
+            }
+
+            // 拡張子指定チェック
+            bool isTargetExtention = false;
+            if( item.ExtentionList != null && item.ExtentionList.Length > 0)
+            {
+                for (int j = 0; j < item.ExtentionList.Length; j++)
+                {
+                    isTargetExtention |= assetPath.Contains(item.ExtentionList[j] );
+                }
+            }
+            else
+            {
+                //指定なし
+                isTargetExtention = true;
+            }
+
+            if( !isTargetExtention)
+            {
+                continue;
+            }
+            targetImporter = item;
+            break;
+        }
+
+        if( targetImporter == null )
+        {
+            return;
+        }
+
+        var importer = assetImporter as ModelImporter;
+		importer.globalScale			= targetImporter.ModelImport.ScaleFactor;
+		importer.useFileScale			= targetImporter.ModelImport.UseFileScale;
+		importer.meshCompression		= targetImporter.ModelImport.MeshCompression;
+		importer.isReadable				= targetImporter.ModelImport.ReadWriteEnabled;
+		importer.optimizeMesh			= targetImporter.ModelImport.OptimizeMesh;
+		importer.importBlendShapes		= targetImporter.ModelImport.ImportBlendShapes;
+		importer.keepQuads				= targetImporter.ModelImport.KeepQuads;
+		importer.weldVertices			= targetImporter.ModelImport.WeldticesVer;
+		importer.importVisibility		= targetImporter.ModelImport.ImportVisibly;
+		importer.importCameras			= targetImporter.ModelImport.ImportCameras;
+		importer.importLights			= targetImporter.ModelImport.ImportLights;
+		importer.swapUVChannels			= targetImporter.ModelImport.SwapUVs;
+		importer.generateSecondaryUV	= targetImporter.ModelImport.GenerateLightmapUVs;
+
+		importer.importNormals			= targetImporter.ModelImport.Normals;
+		importer.importTangents			= targetImporter.ModelImport.Tangents;
+
+		importer.importMaterials		= targetImporter.ModelImport.ImportMaterials;
+		importer.materialName			= targetImporter.ModelImport.MaterialName;
+		importer.materialSearch			= targetImporter.ModelImport.MaterialSearch;
+
+		// Rig
+		importer.animationType			= targetImporter.RigImport.AnimationType;
+		importer.optimizeGameObjects	= targetImporter.RigImport.OptimizeGameObjects;
+
+		// Animations
+		importer.importAnimation		= targetImporter.AnimImport.ImportAnimation;
+    }
+
+    /// <summary>
+    /// 指定のScriptableObject を全取得
+    /// </summary>
+    /// <returns></returns>
+    private CustomFbxImporterScriptableObject[] GetAllTargetScriptableObject() 
+    {
+        List<CustomFbxImporterScriptableObject> ret = new List<CustomFbxImporterScriptableObject>();
+        var guids = UnityEditor.AssetDatabase.FindAssets ("t:CustomFbxImporterScriptableObject");
+        if( guids == null || guids.Length < 1)
+        {
+            return null;
+        }
+
+        for (int i = 0; i < guids.Length; i++)
+        {
+            ret.Add( AssetDatabase.LoadAssetAtPath<CustomFbxImporterScriptableObject>(AssetDatabase.GUIDToAssetPath (guids [i])) );
+        }
+
+        // Path が長い方からチェック(そのための符号反転)
+        ret.Sort( (x,y) =>( -1 * string.Compare( x.Path, y.Path ) ));
+
+        return ret.ToArray();
+    }
+}
